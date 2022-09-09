53b5f7c863629cc00a129344600674cd8e1fad2a
ducknowledges <ducknowledges@gmail.com>
2022-09-09 20:34:11 +0500

    refactor bytestream

--- src/main/java/com/github/ducknowledges/io/bytestreams/CopyBytes.java
+++ src/main/java/com/github/ducknowledges/io/bytestreams/CopyBytes.java
@@ -5,40 +5,51 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class CopyBytes {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         copyBytes();
         copyBytesAutocloseable();
     }
 
-    private static void copyBytes() throws IOException {
+    private static void copyBytes() {
         FileInputStream inputStream = null;
         FileOutputStream outputStream = null;
-
         try {
-            inputStream = new FileInputStream("src/main/resources/input");
-            outputStream = new FileOutputStream("src/main/resources/byteOutput");
+            inputStream = new FileInputStream("input/input");
+            outputStream = new FileOutputStream("output/byteOutput");
             int readedByte;
             while ((readedByte = inputStream.read()) != -1) {
                 outputStream.write(readedByte);
             }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         } finally {
-            if(inputStream != null) {
-                inputStream.close();
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
             }
-            if(outputStream != null) {
-                outputStream.close();
+            if (outputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
             }
         }
     }
 
-    private static void copyBytesAutocloseable() throws IOException {
-        try (
-                FileInputStream inputStream = new FileInputStream("src/main/resources/input");
-                FileOutputStream outputStream = new FileOutputStream("src/main/resources/byteOutput")) {
+    // Using try-with-resources
+    private static void copyBytesAutocloseable() {
+        try (FileInputStream inputStream = new FileInputStream("input/input");
+             FileOutputStream outputStream = new FileOutputStream("output/byteOutput")) {
             int readedByte;
             while ((readedByte = inputStream.read()) != -1) {
                 outputStream.write(readedByte);
             }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
